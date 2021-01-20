e267b5a09dd021a1ae972dd48f0ee236e94f7776
aayushi <aayushisethi100@gmail.com>
2021-01-20 21:17:53 +0530

    changes in layouts

--- app/src/main/java/com/example/hardwarewale/utility/ServerAddress.java
+++ app/src/main/java/com/example/hardwarewale/utility/ServerAddress.java
@@ -2,5 +2,5 @@ package com.example.hardwarewale.utility;
 
 public class ServerAddress {
     //public static String BASE_URL = "http://192.168.43.148:8080";
-    public static String BASE_URL = "https://hardware-wale.herokuapp.com";
+    public static String BASE_URL = "https://hardwarewale.herokuapp.com/";
 }
